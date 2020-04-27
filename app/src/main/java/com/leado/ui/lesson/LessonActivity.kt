0109e55047fe5eb5ab12a3a37824b047fd4a8a63
Ahmed Teleb <ahmedasd37@gmail.com>
2020-04-27 04:32:18 +0200

    add lesson activity

---
+++ app/src/main/java/com/leado/ui/lesson/LessonActivity.kt
@@ -0,0 +1,13 @@
+package com.leado.ui.lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leado.R
+
+class LessonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson)
+    }
+}
