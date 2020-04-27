f17ddb01b9c1bd4464339e78e1df8db9f93a0d3c
Ahmed Teleb <ahmedasd37@gmail.com>
2020-04-27 06:13:20 +0200

    finish lesson api and activity

--- app/src/main/java/com/leado/ui/lesson/LessonActivity.kt
+++ app/src/main/java/com/leado/ui/lesson/LessonActivity.kt
@@ -2,12 +2,35 @@ package com.leado.ui.lesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import com.leado.R
+import kotlinx.android.synthetic.main.activity_lesson.*
 
-class LessonActivity : AppCompatActivity() {
+class LessonActivity : YouTubeBaseActivity() , YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson)
+
+        youtubeplayer_view.initialize(YoutubeConfig.YOUTUBE_API_KEY,this)
+
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        p1?.loadVideo("9YlRIvdokMg")
+
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
     }
 }
