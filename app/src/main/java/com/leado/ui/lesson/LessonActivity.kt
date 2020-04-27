bf45d582c4947e84eabb16bfe0830daf36e741fb
Ahmad Abdelhamid <ahmad.abdelhamid35@gmail.com>
2020-04-27 21:20:51 +0200

    activity lesson added

--- app/src/main/java/com/leado/ui/lesson/LessonActivity.kt
+++ app/src/main/java/com/leado/ui/lesson/LessonActivity.kt
@@ -7,23 +7,23 @@ import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.leado.R
+import com.leado.model.Lesson
 import kotlinx.android.synthetic.main.activity_lesson.*
 
 class LessonActivity : YouTubeBaseActivity() , YouTubePlayer.OnInitializedListener {
 
     val  args : LessonActivityArgs by navArgs()
-
+    lateinit var lesson : Lesson
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson)
-        courseHeader?.courseProgress = 1
-        courseHeader?.lessonNumber = 1
+
+        lesson = args.lesson
         youtubeplayer_view.initialize(YoutubeConfig.YOUTUBE_API_KEY,this)
     }
     override fun onResume() {
         super.onResume()
-        val lesson = args.lesson
-courseHeader?.courseProgress = lesson.id
+        courseHeader?.courseProgress = lesson.id
         courseHeader?.lessonTitle = lesson.title
         tv_lesson_desc.text=lesson.description
     }
@@ -32,12 +32,9 @@ courseHeader?.courseProgress = lesson.id
         p1: YouTubePlayer?,
         p2: Boolean
     ) {
-        p1?.loadVideo("o7d5Zeic63s")
+        p1?.loadVideo(lesson.link)
     }
-    override fun onInitializationFailure(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubeInitializationResult?
-    ) {
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
 
     }
 }
